d8f52c0e629843668fd5ba0a702cbe58377978e6
nicco70 <nicolasgf_98@hotmail.com>
2022-05-31 22:18:32 -0300

    proyecto lista de tareas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './imagenes/lista-tareas.png';
+import ListaDeTareas from './componentes/ListadeTareas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='aplicacion-tareas'>
+      <div className='titulo-lista'>
+        <img
+          src={logo}
+          className='logo-lista' />
+      </div>
+      <div  className='tareas-lista-principal'>
+      <h1>Mis tareas</h1>
+          <ListaDeTareas/>
+      </div>
     </div>
   );
 }
